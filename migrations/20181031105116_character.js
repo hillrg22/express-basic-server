7d95c9b6eda01f1f6d36b0bcb68c83b6d9955097
hillrg22 <hillrg22@gmail.com>
2018-10-31 11:28:10 -0600

    updated migrations, added return statements

--- migrations/20181031105116_character.js
+++ migrations/20181031105116_character.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+  //code to run to set up our tables
+    //create schema
+  return knex.schema.createTable('character', function (table) {
+    table.increments('id')
+    table.string('name')
+    table.integer('height')
+    table.integer('mass')
+  })
+}
 
 exports.down = function(knex, Promise) {
-  
+  // code to run to remove tables & restart
+  return knex.schema.dropTableIfExists('character')
 };
